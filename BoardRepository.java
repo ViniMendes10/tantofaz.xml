8580f1f97cbb9be33989c98b156089cfc1856ee1
ViniMendes10 <168600334+ViniMendes10@users.noreply.github.com>
2025-06-17 16:18:07 -0300

    Create BoardRepository.java

---
+++ BoardRepository.java
@@ -0,0 +1,9 @@
+package com.example.trelloapi.repository;
+
+import com.example.trelloapi.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
